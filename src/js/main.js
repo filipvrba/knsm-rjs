a25e60d5cc9b0497e05dea4395425ea1d70fbe08
Filip Vrba <filipvrba@pm.me>
2024-02-21 16:33:57 +0100

    add bootstrap and json

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,3 @@
+import "../css/bootstrap.min.css";
 import "../css/style.css";
 document.querySelector("#app").innerHTML = "<h1>Hello RubyJS</h1>"
--- src/rb/main.js.rb
+++ src/rb/main.js.rb
@@ -1,3 +1,4 @@
+import '../css/bootstrap.min.css'
 import '../css/style.css'
 
 document.querySelector('#app').innerHTML = "<h1>Hello RubyJS</h1>"
